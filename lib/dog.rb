43c510a570717c40414a0a07217ede5d3a8048fd
Camryn Patterson <camrynpatterson@yahoo.com>
2020-08-31 02:14:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,9 @@
 class Dog
+def bark
+  puts "Woof!"
 end
- 
-fido = Dog.new
 
-"Strings are instances and objects too".reverse
-
-class Dog
+def sit
+  puts "The Dog is sitting"
 end
- 
-fido = Dog.new
-fido.methods
-
-class Dog
-  def bark
-    puts "Woof"
-  end 
 end
-fido.bark 
-
-snoopy = Dog.new
-lassie = Dog.new
-snoopy.bark
-lassie.bark 
